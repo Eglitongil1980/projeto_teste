b849f94e9f10b11470f20ac54050de1787e86136
Eglitongil <egliton@outlook.com>
2024-11-11 19:34:46 -0300

    versãoFinal

--- BotWhatsApp/BotWhatsApp.py
+++ BotWhatsApp/BotWhatsApp.py
@@ -8,9 +8,30 @@ from time import sleep
 ## Código
 
 # Lista dos contatos
-contatos = [5594992238197, 5594981197102]
+# contatos = [5594992238197, 5594981197102]
+
+# # Laço de repetição
+# for telefone in contatos:
+#     webbrowser.open(f'http://api.whatsapp.com/send?phone={telefone}')
+#     sleep(20)
+#     pyautogui.typewrite('Teste de automação, envio de mensagens em massa, via WhatsApp')
+#     sleep(5)
+#     pyautogui.press('enter')
+#     sleep(60)
+
+## Lista de um arquivo .txt
+contatos = []
+
+with open('Contatos.txt','r') as arquivos:
+    for linhas in arquivos:
+        contatos.append(linhas.split('\n')[0])
+    print(contatos)
 
 # Laço de repetição
 for telefone in contatos:
     webbrowser.open(f'http://api.whatsapp.com/send?phone={telefone}')
     sleep(20)
+    pyautogui.typewrite('Teste de automação, envio de mensagens em massa, via WhatsApp')
+    sleep(5)
+    pyautogui.press('enter')
+    sleep(60)
